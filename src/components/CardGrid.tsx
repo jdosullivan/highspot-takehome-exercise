74269a779fb15748a7e7388eb0a2b3cecd64bff2
J OSullivan <jaosulli@gmail.com>
2020-09-07 16:24:26 -0700

    Add the card details

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -17,6 +17,12 @@ function CardGrid(props: ICardListProps) {
                         <Suspense fallback={<img src="" alt="Avatar" style={{ width: "50%" }} />}>
                             <ImageComponent src={card.imageUrl} alt={card.name} />
                         </Suspense>
+                        <div className="card-details">
+                            <div>Name: {card.name}</div>
+                            <div>Text: {card.text}</div>
+                            <div>Set Name: {card.set?.name}</div>
+                            <div>Type: {card.type}</div>
+                        </div>
                     </div>
                 ))}
             </div>
