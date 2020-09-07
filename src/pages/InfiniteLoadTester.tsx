8302540c952fa4e345432f7f63fdfab3320a21d3
J OSullivan <jaosulli@gmail.com>
2020-09-06 20:57:09 -0700

    Fetch all items from elder scrolls api

--- src/pages/InfiniteLoadTester.tsx
+++ src/pages/InfiniteLoadTester.tsx
@@ -1,18 +1,21 @@
 import React, { useCallback, useState, useEffect, Suspense } from "react";
+import { ElderScrollCardsApiResponse } from "../api/ElderScrollApi";
+import { ElderScrollCardModel } from "../core/Models";
 const ImageComponent = React.lazy(() => import("./Image"));
 
 const List = () => {
-    const [listItems, setListItems] = useState([]);
+    const [listItems, setListItems] = useState<ElderScrollCardModel[]>([]);
     const [isFetching, setIsFetching] = useState(false);
     const [page, setPage] = useState(1);
+    const pageSize: number = 20;
 
     const fetchData = useCallback(() => {
         setTimeout(async () => {
-            const result = await fetch(`https://picsum.photos/v2/list?page=${page}`);
-            const data = await result.json();
+            const result = await fetch(`https://api.elderscrollslegends.io/v1/cards?page=${page}&pageSize=${pageSize}`);
+            const data: ElderScrollCardsApiResponse = await result.json();
             setPage(page + 1);
             setListItems((): any => {
-                return [...listItems, ...data];
+                return [...listItems, ...data.cards];
             });
         }, 1000);
     }, [listItems, page]);
@@ -40,15 +43,15 @@ const List = () => {
 
     return (
         <>
-            {listItems.map((listItem: any) => (
+            {listItems.map((listItem: ElderScrollCardModel) => (
                 <div className="card" key={listItem.id}>
                     <Suspense fallback={<img src="https://media.tenor.com/images/b660fe2525e3a20771c924a6cdd16d35/tenor.gif" alt="Avatar" style={{ width: "50%" }} />}>
-                        <ImageComponent src={listItem.download_url} />
+                        <ImageComponent src={listItem.imageUrl} />
                     </Suspense>
 
                     <div className="container">
                         <h4>
-                            <b>{listItem.author}</b>
+                            <b>{listItem.name}</b>
                         </h4>
                         <p>Architect & Engineer</p>
                     </div>
