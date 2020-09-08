24a932d6b3e5e413e078d6d55331e6ea03b7df4c
J OSullivan <jaosulli@gmail.com>
2020-09-07 17:43:39 -0700

    Add brown elderscrolls color

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageHeader() {
+    return (
+        <div className="page-header">
+            <h1>Elder Scrolls Legends</h1>
+        </div>
+    );
+}
+
+export default PageHeader;
